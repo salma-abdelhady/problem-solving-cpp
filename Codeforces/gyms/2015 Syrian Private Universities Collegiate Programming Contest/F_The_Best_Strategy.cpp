876063839f0e8ebe6fb3c803a6cea16dd65b83fc
Salma Abdelhady <salmaabdelhady91@gmail.com>
2026-02-03 21:41:13 +0200

    C, D ,F

---
+++ Codeforces/gyms/2015 Syrian Private Universities Collegiate Programming Contest/F_The_Best_Strategy.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int    long long
+#define vi     vector<int>
+#define pii    pair<int, int>
+#define all(v) v.begin(), v.end()
+#define dpp(arr,val) memset(arr,val,sizeof(arr))
+template<typename T> ostream& operator<<(ostream& os, vector<T>& v){for (auto& i : v) os << i << ' ';return os;}
+template<typename T> istream& operator>>(istream& is, vector<T>& v){for (auto& i : v) is >> i;return is;}
+void FastIO(){ios_base::sync_with_stdio(false);cin.tie(nullptr);}
+void UseFile(){freopen("divsnums.in", "r", stdin);}
+const int MOD = 1000000007;
+const int sz = 2e5+10;
+const double PI = 3.14159265358979323846;
+
+void solve(){
+    int t; cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+
+        vi a(n); cin >> a;
+
+        sort(all(a));
+
+        int cnt = 0;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            if(sum + a[i] <= 300){
+                sum += a[i];
+                cnt ++;
+            }
+            else break;
+        }
+
+        vi pref(cnt);
+        pref[0] = a[0];
+
+        int ans = pref[0];
+
+        for(int i = 1; i < cnt; i++){
+            pref[i] = pref[i-1] + a[i];
+            ans += pref[i];
+        }
+
+        cout << cnt << " " << ans << endl;
+    }
+}
+
+int32_t main(){
+    FastIO();
+    solve();
+    return 0;
+}
